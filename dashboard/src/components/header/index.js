2a1e89b2d9fa6113c8a7bb741a04a188c30d5496
jjanmo <jjanmo07@hanmail.net>
2022-01-02 21:18:36 +0900

    feat: add icon using font-awesome and update styles

--- dashboard/src/components/header/index.js
+++ dashboard/src/components/header/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import styles from './header.module.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faCog, faUser } from '@fortawesome/free-solid-svg-icons';
 const Header = () => {
   return (
     <>
       <div className={styles.container}>
-        <div>Profile</div>
-        <div>Config</div>
-        <div>Notifi</div>
+        <div className={styles.button}>
+          <FontAwesomeIcon icon={faUser} size="2x" />
+        </div>
+        <div className={styles.button}>
+          <FontAwesomeIcon icon={faCog} size="2x" />
+        </div>
+        <div className={styles.button}>
+          <FontAwesomeIcon icon={faBell} size="2x" />
+          <span className={styles.number}>2</span>
+        </div>
       </div>
-      <div className={styles.line} />
     </>
   );
 };
--- dashboard/src/components/item/index.js
+++ dashboard/src/components/item/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './item.module.css';
 
 const Item = ({ text }) => {
@@ -6,8 +8,12 @@ const Item = ({ text }) => {
     <li className={styles.container}>
       <div>{text}</div>
       <div>
-        <button className={styles.doneButton}>Done</button>
-        <button className={styles.deleteButton}>Delete</button>
+        <button className={styles.doneButton}>
+          <FontAwesomeIcon icon={faCheck} size="2x" />
+        </button>
+        <button className={styles.deleteButton}>
+          <FontAwesomeIcon icon={faTimes} size="2x" />
+        </button>
       </div>
     </li>
   );
