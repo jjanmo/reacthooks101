db42bdb5777696e9a2d32c24d91088a80e6cc656
jjanmo <jjanmo07@hanmail.net>
2023-02-06 23:07:34 +0900

    feat: update gnb

--- src/components/gnb/GNB.style.ts
+++ src/components/gnb/GNB.style.ts
@@ -1,9 +1,61 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
-export const Container = styled.div`
+const rotating = keyframes`
+  0%{
+    transform : rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+export const Container = styled.header`
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: 10rem;
+  padding: 0 5rem;
+  margin: 0 auto;
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
 `
 export const List = styled.ul`
   display: flex;
+  justify-self: flex-end;
 `
-export const NavItem = styled.li``
+export const NavItem = styled.li`
+  font-size: 2rem;
+  font-weight: 400;
+  text-transform: uppercase;
+
+  & + & {
+    margin-left: 2.6rem;
+  }
+`
+export const LogoText = styled.div`
+  font-size: 3rem;
+  font-weight: 700;
+  text-transform: uppercase;
+`
+
+export const AnimationContainer = styled.div`
+  position: relative;
+  height: 100%;
+  margin: 0 1rem;
+  /* background-color: red; */
+  flex: 1;
+  display: flex;
+  align-items: center;
+`
+export const LogoWrapper = styled.div`
+  position: absolute;
+  width: 6rem;
+  height: 6rem;
+
+  & svg {
+    animation: ${rotating} 2s linear infinite;
+    transform-origin: center center;
+  }
+`
--- src/components/gnb/GNB.tsx
+++ src/components/gnb/GNB.tsx
@@ -1,20 +1,39 @@
 import Link from 'next/link'
 import * as S from './GNB.style'
+import React from '@icons/react'
+import { useEffect, useRef } from 'react'
 
 export default function GNB() {
+  const animationContainer = useRef<HTMLDivElement>(null)
+
+  // useEffect(() => {
+  //   const animationContainerWidth = animationContainer.current
+  //   console.dir(animationContainerWidth?.offsetWidth)
+  //   console.log(document.documentElement.scrollHeight)
+
+  //   console.log(window.scrollY)
+  // }, [window.scrollY])
+
   return (
     <S.Container>
-      <div>로고</div>
+      <S.LogoText>
+        <Link href="/">reacthooks 101</Link>
+      </S.LogoText>
+      <S.AnimationContainer ref={animationContainer}>
+        <S.LogoWrapper>
+          <React />
+        </S.LogoWrapper>
+      </S.AnimationContainer>
       <S.List>
-        <li>
-          <Link href="/documents">Documents</Link>
-        </li>
-        <li>
-          <Link href="/examples">Examples</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
+        <S.NavItem>
+          <Link href="/documents">documents</Link>
+        </S.NavItem>
+        <S.NavItem>
+          <Link href="/examples">examples</Link>
+        </S.NavItem>
+        <S.NavItem>
+          <Link href="/about">about</Link>
+        </S.NavItem>
       </S.List>
     </S.Container>
   )
