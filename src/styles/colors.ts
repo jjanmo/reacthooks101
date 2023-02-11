bdad4651e4e4986d74534e65c974c2f843874836
jjanmo <jjanmo07@hanmail.net>
2023-02-11 21:21:24 +0900

    feat: add constants colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+export type COLORS_KEY = keyof typeof COLORS
+export const COLORS = {
+  BLUE_100: '#BBDEFB',
+  BLUE_200: '#90CAF9',
+  BLUE_300: '#64B5F6',
+  BLUE_400: '#42A5F5',
+  BLUE_500: '#2196F3',
+  BLUE_600: '#1E88E5',
+  BLUE_700: '#1976D2',
+  BLUE_800: '#1565C0',
+  BLUE_900: '#0D47A1',
+} as const
